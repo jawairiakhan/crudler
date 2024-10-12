208e4c34ebb61d0ad7624e8e3b538775e0fc5211
jawairiakhan <K2115836@kingston.ac.uk>
2024-10-12 13:21:03 +0100

    Completed second video changes (#1)

--- App.js
+++ App.js
@@ -1,22 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ModuleListScreen from './src/components/screens/ModuleListScreen';
+import ModuleAddScreen from './src/components/screens/ModuleAddScreen';
+import ModuleViewScreen from './src/components/screens/ModuleViewScreen';
+import ModuleModifyScreen from './src/components/screens/ModuleModifyScreen';
+
+const Stack = createNativeStackNavigator();
 
 export const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator 
+         initialRouteName = 'ModuleListScreen'
+         screenOptions = {{
+           headerStyle: { backgroundColor: 'black'},
+           headerTintColor: 'white',
+         }}
+      
+      >
+
+       <Stack.Screen 
+          name = 'ModuleListScreen'
+          component = {ModuleListScreen}
+          options = {{title: 'List modules'}}
+       />
+
+        <Stack.Screen 
+          name = 'ModuleAddScreen'
+          component = {ModuleAddScreen}
+          options = {{title: 'Add module'}}
+       />
+
+         <Stack.Screen 
+          name = 'ModuleViewScreen'
+          component = {ModuleViewScreen}
+          options = {{title: 'View module'}}
+        />
+
+         <Stack.Screen 
+          name = 'ModuleModifyScreen'
+          component = {ModuleModifyScreen}
+          options = {{title: 'Modify module'}}
+        />
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default App;
---
+++ src/components/screens/ModuleListScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text } from 'react-native';
+import Screen from '../layout/Screen';
+
+const ModuleListScreen = () => {
+  return (
+   <Screen>
+      <Text>List</Text>
+   </Screen>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ModuleListScreen;
